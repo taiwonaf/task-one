d1586c86f43763f3b5e3c928593021ef2b834525
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2022-10-27 19:18:45 +0100

    App used before initialization corrected

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+const app = express();
+
 app.use(cors({
     origin: '*'
 }))
 
-const app = express();
 
 app.get('/user', (req, res) => (
     res.json({
